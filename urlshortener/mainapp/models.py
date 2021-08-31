40da359305397b2207735b24b53b696ee1977daa
Rafika <RTuremis@hotmail.co.uk>
2021-08-31 15:02:07 +0100

    mainapp added to installed apps, initial db setup made

--- urlshortener/mainapp/models.py
+++ urlshortener/mainapp/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Shortener(models.Model):
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    times_followed = models.PositiveIntegerField(default=0)    
+
+    long_url = models.URLField()
+
+    short_url = models.CharField(max_length=15, unique=True, blank=True)
+
+    class Meta:
+
+        ordering = ["-date_created"]
+
+
+    def __str__(self):
+
+        return f'{self.long_url} to {self.short_url}'
